6801a0496b0871f2ca40cb22b77aea0596c8377b
Elvin Sahbazov <sahbazovelvin92@gmail.com>
2026-07-04 11:36:10 +0400

    Remove Hero sticky h-screen constraint to fix cut-off on small laptops

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -855,7 +855,7 @@ function ForumCountdown() {
 export default function IndexPage() {
   return (
     <div className="min-h-screen bg-black">
-      <div className="md:sticky md:top-0 md:z-0 md:h-screen md:overflow-hidden">
+      <div className="relative z-0">
         <Hero />
       </div>
 
